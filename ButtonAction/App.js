d8bface116870d4ef451b3acdb15e8da24d2d064
Will Hess <willh@aloompa.com>
2018-03-25 18:15:23 -0500

    added second view. TO DO: pass data correctly to the form view

--- ButtonAction/App.js
+++ ButtonAction/App.js
@@ -1,26 +1,22 @@
-
 import React, { Component } from 'react';
-import { SafeAreaView } from 'react-navigation';
-import {
-  Alert,
-  Button,
-  Platform,
-  StyleSheet,
-  TextInput,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import { StackNavigator, SafeAreaView } from 'react-navigation';
 
-export default class App extends Component {
+class Homescreen extends Component {
   constructor(props) {
     super(props);
-    this.state = {text: ''};
+    this.state = { 
+      firstFieldInputText: '', 
+      secondFieldInputText: '',
+      thirdFieldInputText: ''
+    };
   }
 
-  _onPressButton = () =>{
-    const {Text} = this.state;
-       Alert.alert('Inputed Text: ' + Text)
-     }
+  static navigationOptions = { title: 'Homescreen' };
+
+  FunctionToOpenFormView = () => {
+    this.props.navigation.navigate('Second');
+  }
 
   render() {
     return (
@@ -29,35 +25,79 @@ export default class App extends Component {
           style={styles.textInputContainer}
           multiline={true}
           placeholder="Type Here..."
-          color="steelblue"
-          onChangeText={Text => this.setState({Text})}
+          onChangeText={firstFieldInputText => this.setState({ firstFieldInputText })}
+          
+        />
+        <TextInput
+          style={styles.textInputContainer}
+          multiline={true}
+          placeholder="Type Here..."
+          onChangeText={secondFieldInputText => this.setState({ secondFieldInputText })}
+        />
+        <TextInput
+          style={styles.textInputContainer}
+          multiline={true}
+          placeholder="Type Here..."
+          onChangeText={thirdFieldInputText => this.setState({ thirdFieldInputText })}
         />
         <View style={styles.buttonContainer}>
           <Button
-            onPress={this._onPressButton}
+            onPress={this.FunctionToOpenFormView}
             title="Press Me!"
-            color="powderblue"
             fontSize="20"
           />
         </View>
-      </SafeAreaView> 
+      </SafeAreaView>
     );
   }
 }
+
+class FormView extends Component {
+  static navigationOptions = { title: 'FormView' };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.textStyle}> 
+          {this.props.firstFieldInputText}
+        </Text>
+        <Text style={styles.textStyle}>
+          {this.props.secondFieldInputText}
+        </Text>
+        <Text style={styles.textStyle}>
+          {this.props.thirdFieldInputText} 
+        </Text>
+      </View>
+      
+    );
+  }
+}
+
+export default Project = StackNavigator({ 
+  First: { screen: Homescreen }, 
+  Second: { screen: FormView } }
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'powderblue'
+    backgroundColor: '#fff'
+  },
+  textStyle: {
+    fontSize: 23,
+    textAlign: 'center',
+    color: '#000',
+    margin: 20
   },
   textInputContainer: {
     textAlign: 'center',
-    fontSize: 36
+    color: '#000',
+    fontSize: 23,
+    margin: 20
   },
   buttonContainer: {
-    margin: 20,
-    backgroundColor: 'steelblue'
-  }
+    margin: 20
+  },
 });
